85ab52e30d76d5df1f13c339523d7a284a9421d3
Rita Wang <rita.w@wustl.edu>
2025-11-10 14:29:19 -0600

    - added boolean function for X and O player change in xIsNext

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,18 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
